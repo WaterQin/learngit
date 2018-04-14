63a30ebcd0cc7ebb3bfecfd91669a5d8293c71b1
Qinshui <1205395120@qq.com>
2018-04-14 13:00:23 +0800

    Spring

---
+++ Spring/src/main/java/com/service/IMailsendService.java
@@ -0,0 +1,16 @@
+package com.service;
+
+/**
+ * Created by Qinshui on 2017/9/27.
+ */
+public interface IMailsendService {
+    /**
+     *
+     * @Title: sendMessage
+     * @Description: 该方法的主要作用：发送邮件
+     * @param
+     * @return 返回类型：void
+     * @throws
+     */
+    void sendMessage();
+}
---
+++ Spring/src/test/java/Test2.java
@@ -0,0 +1,29 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.mail.MailSender;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * Created by Qinshui on 2017/9/27.
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "classpath:spring/applicationContexct.xml" })
+public class Test2 {
+    @Test
+    public  void test () {
+        //emxwfzpdfcqvfiba//  uqukmbymbpbfgggc
+        ApplicationContext actx = new ClassPathXmlApplicationContext(
+                "classpath:spring/applicationContexct.xml");
+        MailSender sender = (MailSender) actx.getBean("mailSender");
+        SimpleMailMessage mailMessage = (SimpleMailMessage) actx.getBean("mailMessage");
+        mailMessage.setSubject("你好");
+        mailMessage.setText("这个是一个通过Spring框架来发送邮件的小程序");
+        mailMessage.setTo("1205395120@qq.com");
+        sender.send(mailMessage);
+        System.out.println("wziugai bufen sfsdfsdifjfsf ");
+    }
+}
